ea1a7dc6d1c37860380e6dfed80683e48c1b5d24
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-03-21 06:59:38 +0200

    add ticket validation

--- planetarium/models.py
+++ planetarium/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -23,6 +23,10 @@ class PlanetariumDome(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+    @property
+    def capacity(self) -> int:
+        return self.rows * self.seats_in_row
+
     def __str__(self):
         return self.name
 
@@ -40,10 +44,7 @@ class ShowSession(models.Model):
 
     def __str__(self):
         return (
-            f"Show session"
-            f"(id: {self.id}, "
-            f"astronomy show: {self.astronomy_show} "
-            f"planetarium dome: {self.planetarium_dome})"
+            f"{self.astronomy_show} {self.show_time}"
         )
 
 
@@ -66,3 +67,57 @@ class Ticket(models.Model):
         Reservation,
         on_delete=models.CASCADE
     )
+
+    @staticmethod
+    def validate_ticket(
+            row,
+            seat,
+            planetarium_dome,
+            error_to_raise
+    ):
+        for (
+                ticket_attr_value,
+                ticket_attr_name,
+                planetarium_dome_attr_name
+        ) in [
+            (row, "row", "rows"),
+            (seat, "seat", "seats"),
+        ]:
+            count_attrs = getattr(
+                planetarium_dome,
+                planetarium_dome_attr_name
+            )
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise error_to_raise(
+                    {
+                        ticket_attr_name: (
+                            f"{ticket_attr_name} "
+                            f"number must be in available range: "
+                            f"(1, {planetarium_dome_attr_name}): "
+                            f"(1, {count_attrs})"
+                        )
+                    }
+                )
+
+    def clean(self):
+        Ticket.validate_ticket(
+            self.row,
+            self.seat,
+            self.show_session.planetarium_dome,
+            ValidationError,
+        )
+
+    def save(
+        self,
+        force_insert=False,
+        force_update=False,
+        using=None,
+        update_fields=None,
+    ):
+        self.full_clean()
+        return super(Ticket, self).save(
+            force_insert, force_update, using, update_fields
+        )
+
+    def __str__(self):
+        return f"{self.show_session} (row: {self.row}, seat: {self.seat})"
