4b4e3fff0d55bd45304c07defafbd686bb45bccd
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-03-21 15:48:23 +0200

    create planetarium api endpoints

---
+++ planetarium/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket
+)
+
+
+class ShowThemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShowTheme
+        fields = "__all__"
+
+
+class AstronomyShowSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AstronomyShow
+        fields = "__all__"
+
+
+class PlanetariumDomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PlanetariumDome
+        fields = "__all__"
+
+
+class ShowSessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShowSession
+        fields = "__all__"
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reservation
+        fields = "__all__"
+
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = "__all__"
--- planetarium/urls.py
+++ planetarium/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from planetarium.views import TicketViewSet, ShowThemeViewSet, AstronomyShowViewSet, PlanetariumDomeViewSet, \
+    ShowSessionViewSet, ReservationViewSet
+
 app_name = "planetarium"
 
 router = routers.DefaultRouter()
+router.register("themes", ShowThemeViewSet)
+router.register("shows", AstronomyShowViewSet)
+router.register("domes", PlanetariumDomeViewSet)
+router.register("show_sessions", ShowSessionViewSet)
+router.register("reservations", ReservationViewSet)
+router.register("tickets", TicketViewSet)
+
 
 urlpatterns = [path("", include(router.urls))]
