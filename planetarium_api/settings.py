6d8152c288f50ceebbfa3d96e5dd81b665e363dc
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-03-23 03:40:34 +0200

    feat: pagination

--- planetarium_api/settings.py
+++ planetarium_api/settings.py
@@ -145,7 +145,9 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_THROTTLE_CLASSES": ["rest_framework.throttling.AnonRateThrottle",
                                  "rest_framework.throttling.UserRateThrottle"],
-    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "1000/day"}
+    "DEFAULT_THROTTLE_RATES": {"anon": "10/day", "user": "1000/day"},
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 5,
 }
 
 SIMPLE_JWT = {
